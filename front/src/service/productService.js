14c8cc32c2839ff38fcf14087975ad4bff5dbc07
nicolas blasplas <nicolasgorostegui@gmail.com>
2022-02-17 17:37:16 +0100

    datos buenos en la bbdd

--- front/src/service/productService.js
+++ front/src/service/productService.js
@@ -1,4 +1,4 @@
 export function getAllProducts() {
-    return fetch("http://192.168.1.50:5000/api/products", {method:"GET"})
-          .then(res => res.json())
-  }
+    return fetch("http://localhost:5000/api/products", { method: "GET" })
+        .then(res => res.json())
+}
