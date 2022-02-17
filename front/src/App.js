6ab026dbfd759a3f83471c2efca96094d17cc8eb
varanitsa <varanitsa.es@gmail.com>
2022-02-17 11:09:00 +0100

    borrando router para home page

--- front/src/App.js
+++ front/src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import Navbar from "./components/Header";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Profile from "./components/pages/Profile";
-import Categories from "./components/pages/Categories";
-import Home from "./components/pages/Home";
 import Gridproducts from "./components/Startpage/Gridproducts";
 
 
@@ -17,8 +15,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-            <Route path='/' exact component={Home} />
-          <Route path='/Profile' component={Profile} />
+            <Route path='/Profile' component={Profile} />
             <Route path='/Categories' component={Gridproducts} />
         </Switch>
       </Router>
