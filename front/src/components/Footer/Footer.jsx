6f604e30e1327aab4eeb1330b9c6539bfc6aa0d6
jmyc0602 <jmyc0602@gmail.com>
2022-02-17 10:43:46 -0500

    Cambios footer

---
+++ front/src/components/Footer/Footer.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import './footer.css'
+
+
+
+
+
+
+
+
+
+function Footer() {
+  return (
+      <footer className='footer' >
+        <div className='container' >
+          
+            <div className='Company'>
+              <h4>Company</h4>
+              
+                 <p> Buyer</p>
+                 <p> Supplier </p>
+                
+
+                    
+                  
+                
+                
+                
+              
+            </div>
+            <div className='Customers'>
+              <h4>Customers</h4>
+              
+                 <p> About us</p>
+                <p>  Careers</p>
+                <p> Contact us</p> 
+                
+                
+                  
+                
+                
+              
+            </div>
+            <div className='Information'>
+              <h4>Futher Information</h4>
+              
+                  <p>Terms & Conditions </p>
+                   <p> Privacy Policy </p>
+                  
+                
+              
+            </div>
+            <div className='Social'>
+              <h4>Follow us  </h4>
+                <div className='social-links' >
+                   
+                  
+
+
+                </div>
+            </div>
+          </div>
+
+        
+
+
+      </footer>
+  )
+}
+
+export default Footer
