bc5d67e78bb6b680942e96b117e283bc7f003fd6
unknown <patrasioo69@gmail.com>
2022-02-17 14:33:14 +0100

    fixed service card

--- front/src/components/Startpage/Gridproducts.jsx
+++ front/src/components/Startpage/Gridproducts.jsx
@@ -1,85 +1,35 @@
 import React from 'react'
 import './gridproducts.css'
 import Cards from './Cards'
-import Laptop from '../images/laptop-33521_1280.png'
-import Cellphone from '../images/mobile-phone.png'
-import Keyboard from '../images/keyboard.jpg'
-import Cameras from '../images/camera.png'
+// import Laptop from '../images/laptop-33521_1280.png'
+// import Cellphone from '../images/mobile-phone.png'
+// import Keyboard from '../images/keyboard.jpg'
+// import Cameras from '../images/camera.png'
+import { useEffect, useState } from 'react';
+import { getAllProducts } from '../../service/productService';
 
 
 function Gridproducts() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getAllProducts().then((resp) => setProducts(resp));
+  })
+
   return (
+    <div className='container'>
+      <h2 className='titulo'>LAPTOPS</h2>
+        <div className='cardBox'>               
+          {products.map(
+            (product) => (
 
-      <div className='container'>
-         <h2 className='titulo'>LAPTOPS</h2>
-          <div className='cardBox'>               
-     
-      <Cards  img={Laptop} nombre='Laptop'  
-      descripcion='blablabla' precio='$1500'
-       solicitudComprador='acfsdfsd'  />
+              <Cards product={product} key={product.id}/>
 
-    <Cards  img={Laptop} nombre='Laptop'  
-      descripcion='blablabla' precio='$1500'
-       solicitudComprador='acfsdfsd'  />
-
-
-    <Cards  img={Laptop} nombre='Laptop'  
-      descripcion='blablabla' precio='$1500'
-       solicitudComprador='acfsdfsd'  />
-
-              </div>
-
-              
-              <h2 className='titulo'>CELLPHONES</h2>
-          <div className='cardBox'>               
-     
-          <Cards  img={Cellphone} nombre='Cellphone'  
-      descripcion='blablablasacdsadf' precio='$2500'
-       solicitudComprador='hihihih'  />
-
-
-    <Cards  img={Cellphone} nombre='Cellphone'  
-      descripcion='blablablasacdsadf' precio='$2500'
-       solicitudComprador='hihihih'  />
-
-  <Cards img={Cellphone} nombre='Cellphone'  
-      descripcion='blablablasacdsadf' precio='$2500'
-       solicitudComprador='hihihih'  />
-
-              </div>
-
-              <h2 className='titulo'>CAMERAS</h2>
-          <div className='cardBox'>               
-     
-      <Cards  img={Cameras} nombre='Cameras'  
-      descripcion='blablabla' precio='$3500'
-       solicitudComprador='acfsdfsd'  />
-
- <Cards  img={Cameras} nombre='Cameras'  
-      descripcion='blablabla' precio='$3500'
-       solicitudComprador='acfsdfsd'  />
-
-      <Cards  img={Cameras} nombre='Cameras'  
-      descripcion='blablabla' precio='$3500'
-       solicitudComprador='acfsdfsd'  />
-              </div>
-
-              <h2 className='titulo'>KEYBOARDS</h2>
-          <div className='cardBox'>               
-     
-          <Cards img={Keyboard} nombre='keyboard'  
-      descripcion='psappapapa' precio='$500'
-       solicitudComprador='ecdlld'  />
-
-    <Cards img={Keyboard} nombre='keyboard'  
-      descripcion='psappapapa' precio='$500'
-       solicitudComprador='ecdlld'  />
-
-    <Cards img={Keyboard} nombre='keyboard'  
-      descripcion='psappapapa' precio='$500'
-       solicitudComprador='ecdlld'  />
-              </div>
-       </div>
+            )
+          )}
+        </div>
+    </div>
   )
 }
 
